9aad0024a40031dcaf7136987db1f52bb79ed6b4
Alejandro Aguilar Méndez <alejandrooaguilarmendez@gmail.com>
2026-03-18 14:07:45 +0100

    Clientes y Vehículos

--- src/test/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/VehiculosTest.java
+++ src/test/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/VehiculosTest.java
@@ -1,6 +1,7 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
 
 import org.iesalandalus.programacion.tallermecanico.modelo.TallerMecanicoExcepcion;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
